4ec5223170b63af614a1a046bc8ace38bae5c4bd
Fran Martinez <pmg.webdev@gmail.com>
2024-02-18 12:41:17 +0100

    install axios and get request to mock-backend

--- social-slat-app/src/pages/HomePage.jsx
+++ social-slat-app/src/pages/HomePage.jsx
@@ -1,13 +1,33 @@
 
+import { useEffect, useState } from "react";
 import EventsSection from "../components/EventsSection";
 import Hero from "../components/Hero";
+import axios from "axios";
+
+const API_URL = "http://localhost:5000";
 
 
 function HomePage() {
+    const [events, setEvents] = useState([]);
+
+    const getAllEvents = () => {
+        axios   
+          .get(`${API_URL}/events`)
+          .then((response) => {
+            console.log(response.data)
+            setEvents(response.data) })
+          .catch((error) => console.log(error));
+    }
+
+    useEffect(() => {
+        getAllEvents();
+    }, []);
+
     return(
         <> 
             <Hero />
             <EventsSection />
+            <p>{events[0].title}</p>
         
         </>
     );
