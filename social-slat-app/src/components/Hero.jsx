c51a653ef5cf72dc6885c0dd430f354e0669bc0d
Fran Martinez <pmg.webdev@gmail.com>
2024-03-05 12:13:37 +0100

    adds video background on Hero

--- social-slat-app/src/components/Hero.jsx
+++ social-slat-app/src/components/Hero.jsx
@@ -1,8 +1,16 @@
+import HeroVideo from '../assets/video/hero-video.mp4'
+
 function Hero(){
     return(
         <section className="text-gray-600 body-font">
             <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-                <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="Hero Image" src="https://dummyimage.com/720x600" />
+
+                {/* VIDEO */}
+                <div className="absolute  w-full h-full bg-white/10">
+                <video src={HeroVideo} type="video/mp4" autoPlay loop muted className="absolute w-full h-full object-cover -z-10" >
+                </video>
+                </div>
+
                 <div className="text-center lg:w-2/3 w-full">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 fonr-medium text-gray-900">Get Social and Slut</h1>
                     <p className="mb-8 leading-relaxed">Find the event that you need happening around you</p>
