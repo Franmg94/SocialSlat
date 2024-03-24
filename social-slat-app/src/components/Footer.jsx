5efcf735c38ace8b67e8d9b927a0fa1d2b884c69
Fran Martinez <pmg.webdev@gmail.com>
2024-03-24 13:36:56 +0100

    changes logo in header and footer and corrects links

--- social-slat-app/src/components/Footer.jsx
+++ social-slat-app/src/components/Footer.jsx
@@ -1,32 +1,46 @@
-function Footer(){
-    return(
-        <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="#" >
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Social Slat</span>
-                    </a>
-                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                        <li>
-                            <a href="" className="hover:underline me-4 md:me-6">About</a>
-                        </li>
-                        <li>
-                            <a href="" className="hover:underline me-4 md:me-6">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="" className="hover:underline me-4 md:me-6">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="" className="hover:underline">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024<a href="https://flowbite.com/" className="hover:underline">SocialSlat</a>. All Rights Reserved.</span>
-            </div>
-        </footer>
-    );
+import { NavLink } from "react-router-dom";
+import Logo from "../assets/logo.svg";
+NavLink
+
+function Footer() {
+  return (
+    <footer className="bg-white  shadow dark:bg-gray-500 mt-20">
+      <div className="w-full   p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          
+          <a href="#" className="flex gap-1">
+          
+              <img src={Logo} alt="" className="w-10 h-10" />
+              <span className="self-center text-2xl font-semibold text-gray-500 whitespace-nowrap dark:text-white">
+                Social Slat
+              </span>
+         
+          </a>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+              <a href="" className="hover:underline me-4 md:me-6">
+                About
+              </a>
+            </li>
+
+            <li>
+              <a href="" className="hover:underline">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2024{" "}
+          <a href="https://flowbite.com/" className="hover:underline">
+            Social Slat
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- social-slat-app/src/components/Header.jsx
+++ social-slat-app/src/components/Header.jsx
@@ -1,32 +1,28 @@
 import { NavLink } from "react-router-dom";
-
-
+import Logo from "../assets/logo.svg"
 
 function Header() {
   return (
-    <header className="text-gray-600 body-font bg-white sticky top-0 z-10 ">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-pink-500 rounded-full" viewBox="0 0 24 24">
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <NavLink to="/">
-            <span className="ml-3 text-xl">Social Slat</span>
+    <header className="text-gray-600 body-font bg-white sticky top-0 z-20 w-full">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between"> 
+      <img src={Logo} alt="" className="w-10 h-10"/>
+        
+        <NavLink to="/">
+          <span className="ml-3 text-xl">Social Slat</span>
+        </NavLink>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base">
+          <NavLink to="#eventsSection" className="mr-5 hover:text-gray-900">
+            Events
           </NavLink>
-        </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-          <NavLink to="/" >
-          <a href="#eventsSection" className="mr-5 hover:text-gray-900">Events</a>
+          <NavLink to="/users" className="mr-5 hover:text-gray-900">
+            Users
           </NavLink>
-        <NavLink to="/users">
-          <a className="mr-5 hover:text-gray-900">Users</a>
-        </NavLink>
-        <NavLink to="/calendar">
-          <a className="mr-5 hover:text-gray-900">Calendar</a>
-        </NavLink>
-        <NavLink to="/about" >
-          <a className="mr-5 hover:text-gray-900">About</a>
-        </NavLink>
+          <NavLink to="/calendar" className="mr-5 hover:text-gray-900">
+            Calendar
+          </NavLink>
+          <NavLink to="/about" className="mr-5 hover:text-gray-900">
+            About
+          </NavLink>
         </nav>
         <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Sign In
           <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
@@ -36,7 +32,6 @@ function Header() {
       </div>
     </header>
   );
-  
 }
 
 export default Header;
